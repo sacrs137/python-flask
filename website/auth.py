81d5a9e5339c0eab0e3178ae48bc9131e6927d94
sacrs137 <77491046+sacrs137@users.noreply.github.com>
2022-04-10 18:49:48 -0300

    erro de sign up corrigido

--- website/auth.py
+++ website/auth.py
@@ -15,5 +15,24 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    # informações a serem pegas
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        # checando informações do usuário
+        if len(email) < 4:
+            flash('Email precisa conter mais de 4 caracteres.', category='error')
+        elif len(firstName) < 2:
+            flash('Nome precisa conter mais de 1 caracter.', category='error')
+        elif password1 != password2:
+            flash('Senhas diferentes.', category='error')
+        elif len(password1) < 4:
+            flash('Senha precisa conter mais de 4 caracteres.', category='error')
+        else:
+            # seguindo com a inscrição do usuário
+            flash('Conta criada com sucesso!', category='success')
 
     return render_template("sign_up.html")
