02091adb78a0d1013c1ca402e86eb57809748ebd
sacrs137 <77491046+sacrs137@users.noreply.github.com>
2022-04-10 18:46:41 -0300

    Initial commit

---
+++ website/auth.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template, request, flash
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+
+    return render_template("sign_up.html")
