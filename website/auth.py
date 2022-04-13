6c9659f8ca52defba000b006df1ecfda743425ce
sacrs137 <77491046+sacrs137@users.noreply.github.com>
2022-04-12 21:53:43 -0300

    pagina home exclusiva apos login

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -17,14 +18,19 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Login realizado com sucesso!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Algo está errado, tente novamente.', category='error')
     return render_template("login.html")
 
 
 @auth.route('/logout')
+# essa pagina só pode ser acessada se o usuário estiver logado
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
