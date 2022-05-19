ad061375f9d39b649efe1c5ac20430ed9ecc1e36
S <77491046+sviegasdc@users.noreply.github.com>
2022-05-19 18:26:30 -0300

    editar notas implementado

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, render_template, request, jsonify
+from flask import Blueprint, flash, redirect, render_template, request, jsonify, Response
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
@@ -23,6 +23,42 @@ def home():
             flash('Nota criada com sucesso!', category='sucess')
     return render_template("home.html", user=current_user)
 
+@views.route('/edit_note/<int:id>', methods=['GET', 'POST'])
+# essa pagina só pode ser acessada se o usuário estiver logado
+@login_required
+def edit_note(id):
+    update_note = Note.query.get(id)
+    if request.method == "POST":
+        update_note.data = request.form.get('data')
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Ocorreu um erro durante a edição da nota"
+    else:
+        return render_template('edit_note.html', update_note=update_note)
+    # note = Note.query.get(id)
+    # if note is not None:
+    #     if (request.method == 'POST'):
+    #         note = request.form.get('note')
+    #         if len(note) < 1:
+    #             flash('Nota muito curta!', category='error')
+    #         else:
+    #             note = Note(data=note, user_id=current_user.id)
+    #             db.session.commit()
+    #             flash('Nota editada com sucesso!', category='sucess')
+    #             return render_template('home.html',user=current_user, note=note)
+    # else:
+    #     flash('Nota não foi encontrada.', category='error')
+    # return render_template('edit_note.html',user=current_user, note=note)
+    # achando a nota
+ 
+        
+
+
+
+    
+    
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
